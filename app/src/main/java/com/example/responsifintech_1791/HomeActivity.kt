f80cb4811c746c760ff5c5f903b57b994acb5afe
unknown <shalsabillanandasjogja@gmail.com>
2022-04-24 23:49:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/responsifintech_1791/HomeActivity.kt
@@ -0,0 +1,48 @@
+package com.example.responsifintech_1791
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.responsifintech_1791.fragment.HistoryFragment
+import com.example.responsifintech_1791.fragment.HomeFragment
+import com.example.responsifintech_1791.fragment.PaymentFragment
+import com.example.responsifintech_1791.fragment.SettingsFragment
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var home: Button
+    private lateinit var payment: Button
+    private lateinit var history: Button
+    private lateinit var settings: Button
+
+    private fun loadfragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, fragment)
+            .commitNow()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        home = findViewById(R.id.home)
+        payment = findViewById(R.id.payment)
+        history = findViewById(R.id.history)
+        settings = findViewById(R.id.setting)
+
+        home.setOnClickListener{
+            loadfragment(HomeFragment())
+        }
+        payment.setOnClickListener{
+            loadfragment(PaymentFragment())
+        }
+        history.setOnClickListener{
+            loadfragment(HistoryFragment())
+        }
+        settings.setOnClickListener {
+            loadfragment(SettingsFragment())
+        }
+
+    }
+}
